7061e95e9f769030a548b3d029c61bfd5c8b4036
Thanh Huy <akirapanda2002@gmail.com>
2026-09-18 11:15:18 +0700

    implement responsive Navbar component and UI assets

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,17 @@
+
+import cart_icon from './cart_icon.png'
+import dropdown_icon from './dropdown_icon.png'
+import profile_icon from './profile_icon.png'
+import search_icon from './search_icon.png'
+import menu_icon from './menu_icon.png'
+import cross_icon from './cross_icon.png'
+
+export const assets = {
+
+    cart_icon,
+    dropdown_icon,
+    profile_icon,
+    search_icon,
+    menu_icon,
+    cross_icon
+}
