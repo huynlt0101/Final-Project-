86aac2958c793e83cb85fbcdcdb8d8ee2f37da82
Thanh Huy <akirapanda2002@gmail.com>
2026-09-22 10:53:33 +0700

    feat: implement Latest Collection section with ShopContext, product items, and assets

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,4 +1,13 @@
 
+import p_img1 from './p_img1.png'
+import p_img2_1 from './p_img2_1.png'
+import p_img2_2 from './p_img2_2.png'
+import p_img2_3 from './p_img2_3.png'
+import p_img2_4 from './p_img2_4.png'
+import p_img3 from './p_img3.png'
+import p_img4 from './p_img4.png'
+import p_img5 from './p_img5.png'
+
 import cart_icon from './cart_icon.png'
 import dropdown_icon from './dropdown_icon.png'
 import profile_icon from './profile_icon.png'
@@ -18,3 +27,66 @@ export const assets = {
     cross_icon,
     hero_img
 }
+
+export const products = [
+    {
+        _id: "aaaaa",
+        name: "Women V-Neck Knitted Cardigan",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
+        price: 100,
+        image: [p_img1],
+        category: "Women",
+        subCategory: "Topwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "aaaab",
+        name: "Men Stand Collar Zipper Jacket",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
+        price: 200,
+        image: [p_img2_1,p_img2_2,p_img2_3,p_img2_4],
+        category: "Men",
+        subCategory: "Topwear",
+        sizes: ["M", "L", "XL"],
+        date: 1716621345448,
+        bestseller: true
+    },
+    {
+        _id: "aaaac",
+        name: "Girls Knitted Cardigan",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
+        price: 220,
+        image: [p_img3],
+        category: "Kids",
+        subCategory: "Topwear",
+        sizes: ["S", "L", "XL"],
+        date: 1716234545448,
+        bestseller: true
+    },
+    {
+        _id: "aaaad",
+        name: "Men Japanese Vintage T-shirt",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
+        price: 110,
+        image: [p_img4],
+        category: "Men",
+        subCategory: "Topwear",
+        sizes: ["S", "M", "XXL"],
+        date: 1716621345448,
+        bestseller: true
+    },
+    {
+        _id: "aaaae",
+        name: "Women Cowl Neck Lace Long Sleeve T-shirt",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
+        price: 130,
+        image: [p_img5],
+        category: "Women",
+        subCategory: "Topwear",
+        sizes: ["M", "L", "XL"],
+        date: 1716622345448,
+        bestseller: true
+    }
+]
